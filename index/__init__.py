13b0921148f61603d4ce65b631a24f482df84cde
nalejhandro <alejhandro@vauxoo.com>
2021-08-26 15:16:45 -0400

    [ADD]index:Add index app.t#48951

---
+++ index/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2021 Index Datacom, S.A. de C.V
+# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
+from . import models
---
+++ index/models/__init__.py
@@ -0,0 +1,2 @@
+# Copyright 2021 Index Datacom, S.A. de C.V
+# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
